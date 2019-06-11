e71e74149598f800648de2d882fcde936686c76a
loridigia <loridigia@gmail.com>
2019-06-11 18:18:06 +0200

    Init

---
+++ cmake-build-debug/CMakeFiles/PS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PS.dir/main.c.o"
+  "PS.pdb"
+  "PS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
