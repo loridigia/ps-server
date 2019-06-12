e25dd09e5f2e81b05e9eea18431bd38fa846f67b
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-06-12 21:19:52 +0200

    ..

--- main.c
+++ main.c
@@ -1,116 +1,52 @@
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<stdlib.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
-#include<pthread.h> //for threading , link with lpthread
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-//the thread function
-void *connection_handler(void *);
+#define BUFFER_SIZE 1024
+#define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
 
-int main(int argc , char *argv[])
-{
-    int socket_desc , client_sock , c , *new_sock;
-    struct sockaddr_in server , client;
+int main () {
+    int port = 7070;
 
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
+    int server_fd, client_fd, err;
+    struct sockaddr_in server, client;
+    char buf[BUFFER_SIZE];
 
-    //Prepare the sockaddr_in structure
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) on_error("Could not create socket\n");
+
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
+    //int opt_val = 1;
+    //setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
+
+    err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
+    if (err < 0) on_error("Could not bind socket\n");
+
+    err = listen(server_fd, 128);
+    if (err < 0) on_error("Could not listen on socket\n");
+
+    printf("Server is listening on %d\n", port);
+
+    while (1) {
+        socklen_t client_len = sizeof(client);
+        client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
+
+        if (client_fd < 0) on_error("Could not establish new connection\n");
+
+            int read = recv(client_fd, buf, BUFFER_SIZE, 0);
+
+            //if (!read) break; // done reading
+            if (read < 0) on_error("Client read failed\n");
+
+            err = send(client_fd, buf, read, 0);
+            if (err < 0) on_error("Client write failed\n");
     }
-    puts("bind done");
-
-    //Listen
-    listen(socket_desc , 3);
-
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-
-
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-        puts("Connection accepted");
-
-        pthread_t sniffer_thread;
-        new_sock = malloc(1);
-        *new_sock = client_sock;
-
-        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-        {
-            perror("could not create thread");
-            return 1;
-        }
-
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
-        puts("Handler assigned");
-    }
-
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-
-    return 0;
-}
-
-/*
- * This will handle connection for each client
- * */
-void *connection_handler(void *socket_desc)
-{
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    char *message , client_message[2000];
-
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
-
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
-
-    //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-    {
-        //Send the message back to client
-        write(sock , client_message , strlen(client_message));
-    }
-
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-
-    //Free the socket pointer
-    free(socket_desc);
 
     return 0;
 }
