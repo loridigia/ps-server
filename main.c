7dcda963737b40ed54b2e55b26abd8cc1d300553
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-06-29 11:39:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,156 @@
+/*
+ * server.c
+ * Version 20161003
+ * Written by Harry Wong (RedAndBlueEraser)
+ */
+
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
-#define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
+#define BACKLOG 10
 
-int main () {
-    int port = 7070;
+typedef struct pthread_arg_t {
+    int new_socket_fd;
+    struct sockaddr_in client_address;
+    /* TODO: Put arguments passed to threads here. See lines 116 and 139. */
+} pthread_arg_t;
 
-    int server_fd, client_fd, err;
-    struct sockaddr_in server, client;
-    char buf[BUFFER_SIZE];
+/* Thread routine to serve connection to client. */
+void *pthread_routine(void *arg);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) on_error("Could not create socket\n");
+/* Signal handler to handle SIGTERM and SIGINT signals. */
+void signal_handler(int signal_number);
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+int main(int argc, char *argv[]) {
+    int port, socket_fd, new_socket_fd;
+    struct sockaddr_in address;
+    pthread_attr_t pthread_attr;
+    pthread_arg_t *pthread_arg;
+    pthread_t pthread;
+    socklen_t client_address_len;
 
-    //int opt_val = 1;
-    //setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof opt_val);
-
-    err = bind(server_fd, (struct sockaddr *) &server, sizeof(server));
-    if (err < 0) on_error("Could not bind socket\n");
-
-    err = listen(server_fd, 128);
-    if (err < 0) on_error("Could not listen on socket\n");
-
-    printf("Server is listening on %d\n", port);
-
-    while (1) {
-        socklen_t client_len = sizeof(client);
-        client_fd = accept(server_fd, (struct sockaddr *) &client, &client_len);
-
-        if (client_fd < 0) on_error("Could not establish new connection\n");
-
-            int read = recv(client_fd, buf, BUFFER_SIZE, 0);
-
-            //if (!read) break; // done reading
-            if (read < 0) on_error("Client read failed\n");
-
-            err = send(client_fd, buf, read, 0);
-            if (err < 0) on_error("Client write failed\n");
+    /* Get port from command line arguments or stdin. */
+    port = argc > 1 ? atoi(argv[1]) : 0;
+    if (!port) {
+        printf("Enter Port: ");
+        scanf("%d", &port);
     }
 
+    /* Initialise IPv4 address. */
+    memset(&address, 0, sizeof address);
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    /* Create TCP socket. */
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    /* Bind address to socket. */
+    if (bind(socket_fd, (struct sockaddr *)&address, sizeof address) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    /* Listen on socket. */
+    if (listen(socket_fd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    /* Assign signal handlers to signals. */
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+
+    /* Initialise pthread attribute to create detached threads. */
+    if (pthread_attr_init(&pthread_attr) != 0) {
+        perror("pthread_attr_init");
+        exit(1);
+    }
+    if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+        perror("pthread_attr_setdetachstate");
+        exit(1);
+    }
+
+    while (1) {
+        /* Create pthread argument for each connection to client. */
+        /* TODO: malloc'ing before accepting a connection causes only one small
+         * memory when the program exits. It can be safely ignored.
+         */
+        pthread_arg = (pthread_arg_t *)malloc(sizeof *pthread_arg);
+        if (!pthread_arg) {
+            perror("malloc");
+            continue;
+        }
+
+        /* Accept connection to client. */
+        client_address_len = sizeof pthread_arg->client_address;
+        new_socket_fd = accept(socket_fd, (struct sockaddr *)&pthread_arg->client_address, &client_address_len);
+        if (new_socket_fd == -1) {
+            perror("accept");
+            free(pthread_arg);
+            continue;
+        }
+
+        /* Initialise pthread argument. */
+        pthread_arg->new_socket_fd = new_socket_fd;
+        /* TODO: Initialise arguments passed to threads here. See lines 22 and
+         * 139.
+         */
+
+        /* Create thread to serve connection to client. */
+        if (pthread_create(&pthread, &pthread_attr, pthread_routine, (void *)pthread_arg) != 0) {
+            perror("pthread_create");
+            free(pthread_arg);
+            continue;
+        }
+    }
+
+    /* close(socket_fd);
+     * TODO: If you really want to close the socket, you would do it in
+     * signal_handler(), meaning socket_fd would need to be a global variable.
+     */
     return 0;
+}
+
+void *pthread_routine(void *arg) {
+    pthread_arg_t *pthread_arg = (pthread_arg_t *)arg;
+    int new_socket_fd = pthread_arg->new_socket_fd;
+    struct sockaddr_in client_address = pthread_arg->client_address;
+    /* TODO: Get arguments passed to threads here. See lines 22 and 116. */
+
+    free(arg);
+
+    /* TODO: Put client interaction code here. For example, use
+     * write(new_socket_fd,,) and read(new_socket_fd,,) to send and receive
+     * messages with the client.
+     */
+
+
+    close(new_socket_fd);
+    return NULL;
+}
+
+void signal_handler(int signal_number) {
+    /* TODO: Put exit cleanup code here. */
+    exit(0);
 }
