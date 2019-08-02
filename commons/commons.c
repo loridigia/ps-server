bda7126c4348c9a7bbee150eb7b7a5968f1bd0ad
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-08-02 22:27:16 +0200

    ..

--- commons/commons.c
+++ commons/commons.c
@@ -1,18 +1,11 @@
-#include <netinet/in.h>
-#include <pthread.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <ifaddrs.h>
-#include <sys/file.h>
 #include <sys/mman.h>
 #include "commons.h"
 
