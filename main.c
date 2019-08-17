a380b9936ea4863dd8a511113f1fbb6d90ee4000
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-08-17 11:19:51 +0200

    daemon

--- main.c
+++ main.c
@@ -54,54 +54,70 @@ pthread_cond_t condition;
 int pipe_fd[2];
 
 int main(int argc, char *argv[]) {
-    config = mmap(NULL, sizeof config, PROT_READ | PROT_WRITE,
-                  MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-    if (load_configuration(COMPLETE) == -1 || load_arguments(argc,argv) == -1) {
+    pid_t process_id = 0;
+    pid_t sid = 0;
+    process_id = fork();
+
+    if (process_id < 0) {
+        printf("fork failed!\n");
         exit(1);
+    } else if (process_id == 0) {
+        int pid = getpid(); //debugging purpose
+        fprintf(stdout,"Server running...\n PID -> %d\n",pid);
+        sid = setsid();
+        if(sid < 0) {
+            exit(1);
+        }
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        config = mmap(NULL, sizeof config, PROT_READ | PROT_WRITE,
+                      MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+        if (load_configuration(COMPLETE) == -1 || load_arguments(argc,argv) == -1) {
+            exit(1);
+        }
+
+        if (pthread_attr_init(&pthread_attr) != 0) {
+            perror("Errore nell'inizializzazione degli attributi del thread.\n");
+            exit(1);
+        }
+
+        if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+            perror("Errore nel settare DETACHED_STATE al thread.\n");
+            exit(1);
+        }
+
+        if (pipe(pipe_fd) == -1){
+            perror("Errore nel creare la pipe.\n");
+            exit(1);
+        }
+
+        pthread_mutexattr_t mutexAttr;
+        pthread_mutexattr_setpshared(&mutexAttr, PTHREAD_PROCESS_SHARED);
+        pthread_mutex_init(&mutex, &mutexAttr);
+
+        pthread_condattr_t condAttr;
+        pthread_condattr_setpshared(&condAttr, PTHREAD_PROCESS_SHARED);
+        pthread_cond_init(&condition, &condAttr);
+
+        pid_t pid_child = fork();
+
+        if (pid_child < 0) {
+            perror("Errore durante la fork.\n");
+            exit(0);
+        } else if (pid_child == 0) {
+            write_log();
+            exit(0);
+        }
+
+        start();
+        signal(SIGHUP, restart);
+        while(1) sleep(1);
     }
-
-    int pid = getpid(); //debugging purpose
-    fprintf(stdout,"Pid:%d\n",pid);
-
-    if (pthread_attr_init(&pthread_attr) != 0) {
-        perror("Errore nell'inizializzazione degli attributi del thread.\n");
-        exit(1);
-    }
-
-    if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
-        perror("Errore nel settare DETACHED_STATE al thread.\n");
-        exit(1);
-    }
-
-    if (pipe(pipe_fd) == -1){
-        perror("Errore nel creare la pipe.\n");
-        exit(1);
-    }
-
-    pthread_mutexattr_t mutexAttr;
-    pthread_mutexattr_setpshared(&mutexAttr, PTHREAD_PROCESS_SHARED);
-    pthread_mutex_init(&mutex, &mutexAttr);
-
-    pthread_condattr_t condAttr;
-    pthread_condattr_setpshared(&condAttr, PTHREAD_PROCESS_SHARED);
-    pthread_cond_init(&condition, &condAttr);
-
-    pid_t pid_child = fork();
-
-    if (pid_child < 0) {
-        perror("Errore durante la fork.\n");
-        exit(0);
-    } else if (pid_child == 0) {
-        write_log();
-        exit(0);
-    }
-
-    start();
-    signal(SIGHUP, restart);
-    while(1) {
-        sleep(1);
-    }
+    exit(0);
 }
 
 /*
@@ -286,7 +302,8 @@ void *send_routine(void *arg) {
         }
     } if (equals(config->type,"process")) {
         exit(0);
-    } return NULL;
+    }
+    return NULL;
 }
 
 int serve_client(int client_fd, char *client_ip, int port) {
