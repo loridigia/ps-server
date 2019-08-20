a11ea565fde01230b77d674c7326e0a5e4624d64
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-08-20 16:59:14 +0200

    struttura definitiva credo

--- core/core.h
+++ core/core.h
@@ -4,6 +4,10 @@ int send_error(int socket_fd, char *err);
 int send_file(int socket_fd, char *file, size_t file_size);
 char *get_client_ip(struct sockaddr_in *socket_addr);
 char *get_client_buffer(int client_fd, int *err);
+int serve_client(int client_fd, char *client_ip, int port);
+int work_with_threads(int fd, char *client_ip, int port);
+int work_with_processes(int fd, char *client_ip, int port);
+void handle_requests(int port, int (*handle)(int fd, char* client_ip, int port));
 
 /* --------------------------- WINDOWS --------------------------- */
 int listen_on();
@@ -11,9 +15,16 @@ int send_error();
 int send_file();
 char *get_client_ip();
 char *get_client_buffer();
+int serve_client();
+int work_with_threads();
+int work_with_processes();
+void handle_requests();
 
 /* --------------------------- COMMONS --------------------------- */
 char *get_server_ip();
 void start();
 void log_routine();
 void init(int argc, char *argv[]);
+void *pthread_receiver_routine(void *arg);
+void *pthread_listener_routine(void *arg);
+int write_on_pipe(int size, char *name, int port, char *ip);
