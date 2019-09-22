ab4cfafcfad1971aa1fce69590fa77abbedb9824
jacopoRufini <jacoporufini97@gmail.com>
2019-09-22 14:49:02 +0200

    Update receiver.c

--- win/receiver.c
+++ win/receiver.c
@@ -6,9 +6,8 @@
 extern configuration *config;
 
 int main(int argc, char *argv[]) {
-    WSAPROTOCOL_INFO protocolInfo;
-    DWORD dwRead;
-    HANDLE hStdin, hStdout;
+    WSAPROTOCOL_INFO protocol_info;
+    HANDLE stdin_handle, stdout_handle;
 
     int port = atoi(argv[0]);
     char *ip = argv[1];
@@ -25,29 +24,42 @@ int main(int argc, char *argv[]) {
 
     h_pipe = CreateFile(PIPENAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
     if (h_pipe == INVALID_HANDLE_VALUE && GetLastError() != ERROR_PIPE_BUSY){
-        perror("Errore nella creazione della pipe");
+        print_error("Errore nella creazione della pipe");
         exit(1);
     }
 
     freopen("CONOUT$", "w", stderr);
     freopen("CONOUT$", "w", stdout);
 
-    hStdout = CreateFile("CONOUT$",  GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    hStdin = GetStdHandle(STD_INPUT_HANDLE);
-    if ((hStdout == INVALID_HANDLE_VALUE) || (hStdin == INVALID_HANDLE_VALUE)){
-        perror("invalid handles");
+    stdout_handle = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (stdout_handle == INVALID_HANDLE_VALUE) {
+        print_error("stdout_handle non valido");
         exit(1);
     }
 
-    SetStdHandle(STD_OUTPUT_HANDLE, hStdout);
-    SetStdHandle(STD_ERROR_HANDLE, hStdout);
+    stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
+    if (stdin_handle == INVALID_HANDLE_VALUE) {
+        print_error("stdin_handle non valido");
+        exit(1);
+    }
+
+    if (SetStdHandle(STD_OUTPUT_HANDLE, stdout_handle) == 0 ) {
+        print_error("Impossibile settare stdout_handle");
+        exit(1);
+    }
+
+    if (SetStdHandle(STD_ERROR_HANDLE, stdout_handle) == 0 ) {
+        print_error("Impossibile settare stderr_handle");
+        exit(1);
+    }
 
     WSADATA wsaData;
-    WORD wVersionRequested = MAKEWORD( 2, 0 );
+    WORD wVersionRequested = MAKEWORD(2,0);
     WSAStartup( wVersionRequested, &wsaData );
 
-    ReadFile(hStdin, &protocolInfo, sizeof(protocolInfo), &dwRead, NULL);
-    SOCKET socket = WSASocket(protocolInfo.iAddressFamily, protocolInfo.iSocketType, protocolInfo.iProtocol, &protocolInfo, 0, WSA_FLAG_OVERLAPPED);
+    DWORD dw_read;
+    ReadFile(stdin_handle, &protocol_info, sizeof(protocol_info), &dw_read, NULL);
+    SOCKET socket = WSASocket(protocol_info.iAddressFamily, protocol_info.iSocketType, protocol_info.iProtocol, &protocol_info, 0, WSA_FLAG_OVERLAPPED);
 
     if (socket == INVALID_SOCKET) {
         printf("errore socket: %lu", WSAGetLastError());
